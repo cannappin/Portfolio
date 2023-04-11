fe65ff9f498f7ce350f2ba23f7fbeeaeeb70e542
cannappin <gokoolcannappin@gmail.com>
2023-04-11 13:28:54 +0200

    End of navigation

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import {FaBars, FaTimes} from 'react-icons/fa'
 
 function Header() {
 
-    const [nav, setNav] = useState(false) ;
+    const [nav , setNav] = useState(false) ;
     const links = [
         {
             id :1, 
@@ -34,21 +34,30 @@ function Header() {
     ]
 
     return (
-        <header className="bg-gray-900 text-white py-4 px-8 flex justify-between items-center">
+        <header className="bg-gray-100 text-gray-500 py-4 px-8 flex justify-between items-center">
             <h1 className="text-5xl font-signature cursor-pointer"><a href="/">Gokool</a></h1>
             <nav>
-                <ul className="hidden md:flex space-x-6 ">
+                <ul className="hidden md:flex space-x-8 ">
                     {links.map(({id, link}) => (
-                        <li key={id} className='hover:scale-105 duration-200 hover:text-gray-400'><a href="/">{link}</a></li>                    
+                        <li key={id} className='hover:scale-110 duration-200 hover:text-gray-400'><a href="/">{link}</a></li>                    
                     ))}
                 </ul>
 
-                <div onClick={() => setNav(!nav)}
-                    className="cursor-pointer pr-4 z-10 text-white">
-                    {nav ? <FaTimes size={30}/> : <FaBars size={30} />}
-                </div>
+                    {nav && ( 
 
+                        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-cyan-900">
+                    {links.map(({id, link}) => (
+                        <li key={id} className='px-4 cursor-pointer capitalize py-6 text-5xl'><a href="/">{link}</a></li>  
+                        ))}
+                        </ul>
+                    )}          
             </nav>
+
+            <div onClick={() => setNav(!nav)}
+                className="cursor-pointer pr-4 z-10  text-white  md:hidden ">
+                { nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+            
         </header>
     );
 }
